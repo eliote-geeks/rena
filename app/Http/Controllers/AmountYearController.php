6b061137404a09e63bf586a3d4c3884d2d0041ac
eliote-geeks <pauleliote97@gmail.com>
2024-07-22 14:26:58 +0200

    montage des tables

---
+++ app/Http/Controllers/AmountYearController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AmountYear;
+use Illuminate\Http\Request;
+
+class AmountYearController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(AmountYear $amountYear)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(AmountYear $amountYear)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, AmountYear $amountYear)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(AmountYear $amountYear)
+    {
+        //
+    }
+}
---
+++ database/migrations/2024_07_22_115038_create_mutualists_table.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mutualists', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('first')->nullable();
+            $table->string('last')->nullable();
+            $table->string('sexe')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('country')->nullable();
+            $table->string('birth_date')->nullable();
+            $table->string('place_birth')->nullable();
+            $table->string('work')->nullable();
+            $table->string('localisation')->nullable();
+            $table->string('repere')->nullable();
+            $table->string('identification_type')->nullable();
+            $table->string('num_identification')->nullable();
+            $table->string('date_delivry')->nullable();
+            $table->string('date_delivry')->nullable();
+            $table->string('matrimonial')->nullable();
+            $table->string('beneficiary')->nullable();
+            $table->string('beneficiary_1')->nullable();
+            $table->string('beneficiary_2')->nullable();
+            $table->string('beneficiary_3')->nullable();
+            $table->string('beneficiary_4')->nullable();
+            $table->string('beneficiary_5')->nullable();
+            $table->string('beneficiary_6')->nullable();
+            $table->string('beneficiary_7')->nullable();
+            $table->string('beneficiary_8')->nullable();
+            $table->string('beneficiary_9')->nullable();
+            $table->string('beneficiary_10')->nullable();
+            $table->longText('id_card_smart')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mutualists');
+    }
+};
